ecb181b1f91ce41b3e6bddd4d48f37cc841972a4
nagievr <ruslannagiev96@gmail.com>
2021-04-22 00:05:49 +0300

    add some data

--- scripts/main.js
+++ scripts/main.js
@@ -16,15 +16,35 @@ const app = {
       product: {
         name:'Socks SB',
         date:'29.92/2019',
-        // foo() {
-        //   setTimeout(() => {
-        //     this.name = 'Socks UA';
-        //   }, 2000)
-        // },
         imageSrc: './assets/socks1.jpg',
         inStock: true,
-      }
+        details: ['50% cotton', '10% carton', '40% air'],
+        variants: [
+          { name: 'blue', src: 'https://dodosocks.com/wp-content/uploads/2017/03/kunsht_01-2-800x800.jpg' }, 
+          { name: 'green', src: './assets/socks1.jpg'}
+        ]
+      },
+
+      styles: {
+        backgroundColor: 'gray',
+        width: '100px',
+        cursor: 'pointer',
+        border: '1px sodlid red'
+      },
+
+      card: 0,
       
+    }
+  },
+
+  methods: {
+    addToCard() {
+      this.card++;
+    },
+
+    updateImage(src) {
+      console.log(src);
+      this.product.imageSrc = src;
     }
   }
 }
